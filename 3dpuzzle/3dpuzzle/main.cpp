30dbe8c293c7bc6ab02d77e5999f676a7a47a808
edmond <darkshooter@gmail.com>
2014-06-19 00:55:07 -0400

    lecture de la matrice marche

--- 3dpuzzle/3dpuzzle/main.cpp
+++ 3dpuzzle/3dpuzzle/main.cpp
@@ -4,9 +4,93 @@
 using namespace std;
 
 
+// globals
+int dimension = 0; //3,4,5 etc
+
+struct cell
+{
+	vector<cell*> voisins;
+	char color;
+
+};
+
+
+vector<cell> all_cells;
+
+
+string itos(int integer)
+{
+	char ascii[100];
+	sprintf(ascii, "%d", integer);
+	string s = ascii;
+	return s;
+}
+
+void readMatrixLine(char* line)
+{
+
+	char *pc = line;
+	while (1)
+	{
+		//end of line
+		if (*pc == 10) break;
+
+		if (*pc == ' ') {
+			pc++;
+			continue;
+		}
+
+		cell t;
+		t.color = *pc;
+		all_cells.push_back(t);
+		pc++;
+
+	}
+
+
+}
+
+void readGraphFile(char * filename)
+{
+
+	FILE * fichier = fopen(filename, "r");
+	static char line[200];
+
+	char * pc; //iterator
+
+	//Read the dimension
+
+	while (fgets(line, 200, fichier) != NULL) 
+	{
+
+		//Commentary, skip
+		if (line[0] == ';') continue;
+
+		//Empty line, skip
+		if (line[0] == 10) continue;
+
+		//Special case for the dimension
+		if (line[0] == 'd') {
+			dimension = line[1] + '0';
+			continue;
+		}
+
+		pc = line;
+
+		//Read a line of the matrix
+		//readMatrixLine(pc);
+
+		readMatrixLine(pc);
+
+	}
+}
+
+
 int main(void)
 {
 	
+	//Read the puzzle
+	readGraphFile("puzzle.txt");
 
 
 
